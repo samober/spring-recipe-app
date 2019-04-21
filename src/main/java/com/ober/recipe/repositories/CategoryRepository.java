6a609e8de1d3906e257e03ddc7549d66732f4cba
Sam Ober <ober.sam@gmail.com>
2019-04-20 21:32:11 -0400

    Adding recipe bootstrap code

--- src/main/java/com/ober/recipe/repositories/CategoryRepository.java
+++ src/main/java/com/ober/recipe/repositories/CategoryRepository.java
@@ -3,5 +3,10 @@ package com.ober.recipe.repositories;
 import com.ober.recipe.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
+
 }
--- src/main/java/com/ober/recipe/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/ober/recipe/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,10 @@ package com.ober.recipe.repositories;
 import com.ober.recipe.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
+
 }
