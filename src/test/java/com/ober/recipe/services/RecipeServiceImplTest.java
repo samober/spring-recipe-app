e1cddc649b423421cc5e177a37a322723ac14a1c
Sam Ober <ober.sam@gmail.com>
2019-04-29 19:25:21 -0400

    Adding bootstrap and jquery webjars

---
+++ src/test/java/com/ober/recipe/services/RecipeServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.ober.recipe.services;
+
+import com.ober.recipe.domain.Recipe;
+import com.ober.recipe.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        Mockito.when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(1, recipes.size());
+
+        Mockito.verify(recipeRepository, Mockito.times(1)).findAll();
+    }
+}
