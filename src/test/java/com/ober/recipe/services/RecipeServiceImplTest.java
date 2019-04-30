e8948dc9cda58660aeb7fbc55e14f0538e04c2ed
Sam Ober <ober.sam@gmail.com>
2019-04-29 20:21:04 -0400

    Adding in show recipe page

--- src/test/java/com/ober/recipe/services/RecipeServiceImplTest.java
+++ src/test/java/com/ober/recipe/services/RecipeServiceImplTest.java
@@ -9,9 +9,11 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class RecipeServiceImplTest {
 
@@ -28,16 +30,31 @@ public class RecipeServiceImplTest {
     }
 
     @Test
+    public void getRecipebyIdTest() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+
+        assertNotNull("Null recipe returned", recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+
+    @Test
     public void getRecipes() {
         Recipe recipe = new Recipe();
         HashSet recipesData = new HashSet();
         recipesData.add(recipe);
 
-        Mockito.when(recipeRepository.findAll()).thenReturn(recipesData);
+        when(recipeRepository.findAll()).thenReturn(recipesData);
 
         Set<Recipe> recipes = recipeService.getRecipes();
         assertEquals(1, recipes.size());
 
-        Mockito.verify(recipeRepository, Mockito.times(1)).findAll();
+        verify(recipeRepository, times(1)).findAll();
     }
 }
