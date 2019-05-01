00696dcfb5df44f51673e391e68f6d68b8312984
Sam Ober <ober.sam@gmail.com>
2019-05-01 17:48:51 -0400

    Adding 404 error page to recipe controller

--- src/main/java/com/ober/recipe/controllers/RecipeController.java
+++ src/main/java/com/ober/recipe/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package com.ober.recipe.controllers;
 
 import com.ober.recipe.commands.RecipeCommand;
+import com.ober.recipe.exceptions.NotFoundException;
 import com.ober.recipe.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -47,6 +50,15 @@ public class RecipeController {
         log.debug("Deleting RecipeID: " + id);
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound() {
+        log.error("Handling not found exception");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+        return modelAndView;
     }
 
 }
