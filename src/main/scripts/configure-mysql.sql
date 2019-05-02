6e20dfd46c34b375bfc81e2530a3193568a0773c
Sam Ober <ober.sam@gmail.com>
2019-05-02 19:47:27 -0400

    Updating MySQL configuration script

--- src/main/scripts/configure-mysql.sql
+++ src/main/scripts/configure-mysql.sql
@@ -5,6 +5,8 @@ CREATE DATABASE recipe_prod;
 # Create database service accounts
 CREATE USER 'recipe_dev_user'@'localhost' IDENTIFIED BY 'recipe';
 CREATE USER 'recipe_prod_user'@'localhost' IDENTIFIED BY 'recipe';
+CREATE USER 'recipe_dev_user'@'%' IDENTIFIED BY 'recipe';
+CREATE USER 'recipe_prod_user'@'%' IDENTIFIED BY 'recipe';
 
 # Database grants
 GRANT SELECT ON recipe_dev.* TO 'recipe_dev_user'@'localhost';
@@ -15,3 +17,11 @@ GRANT SELECT ON recipe_prod.* TO 'recipe_prod_user'@'localhost';
 GRANT INSERT ON recipe_prod.* TO 'recipe_prod_user'@'localhost';
 GRANT DELETE ON recipe_prod.* TO 'recipe_prod_user'@'localhost';
 GRANT UPDATE ON recipe_prod.* TO 'recipe_prod_user'@'localhost';
+GRANT SELECT ON recipe_dev.* TO 'recipe_dev_user'@'%';
+GRANT INSERT ON recipe_dev.* TO 'recipe_dev_user'@'%';
+GRANT DELETE ON recipe_dev.* TO 'recipe_dev_user'@'%';
+GRANT UPDATE ON recipe_dev.* TO 'recipe_dev_user'@'%';
+GRANT SELECT ON recipe_prod.* TO 'recipe_prod_user'@'%';
+GRANT INSERT ON recipe_prod.* TO 'recipe_prod_user'@'%';
+GRANT DELETE ON recipe_prod.* TO 'recipe_prod_user'@'%';
+GRANT UPDATE ON recipe_prod.* TO 'recipe_prod_user'@'%';
